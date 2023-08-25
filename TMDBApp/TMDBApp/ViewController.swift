f3cf8d67b0a83fa703c2f3a3d7d2de6729769d81
isGeekCode <ptuguitar@naver.com>
2023-08-26 02:50:09 +0900

    [CHORE] TabBarController Set

--- TMDBApp/TMDBApp/ViewController.swift
+++ TMDBApp/TMDBApp/ViewController.swift
@@ -12,8 +12,31 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = .white
     }
-
+    
 
 }
 
+
+class ViewControllerB: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemYellow
+    }
+}
+
+
+class TabBarController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let firstVC = ViewController()
+        firstVC.tabBarItem = UITabBarItem(title: "Movie", image: UIImage(systemName: "bookmark.fill"), tag: 0)
+        
+        let secondVC = ViewControllerB()
+        secondVC.tabBarItem = UITabBarItem(title: "TV", image: UIImage(systemName: "tv"), tag: 1)
+
+        viewControllers = [firstVC, secondVC]
+    }
+}
